240859c59bf9e13eb18984789ff6785835c99635
tss-ku <tss.ku@yahoo.com>
2026-01-27 16:57:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -64,3 +64,25 @@ let interval = setInterval(() => {
       desktopMenu.style.display = 'none';
     }
   });
+//Hover for Emial and Call Us button
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".js-hover-swap");
+
+  buttons.forEach((btn) => {
+    const defaultText = btn.dataset.default;
+    const hoverText = btn.dataset.hover;
+    const defaultColor = btn.dataset.defaultColor;
+    const hoverColor = btn.dataset.hoverColor;
+
+    btn.addEventListener("mouseenter", () => {
+      btn.textContent = hoverText;
+      btn.style.color = hoverColor;
+    });
+
+    btn.addEventListener("mouseleave", () => {
+      btn.textContent = defaultText;
+      btn.style.color = defaultColor;
+    });
+  });
+});
+
